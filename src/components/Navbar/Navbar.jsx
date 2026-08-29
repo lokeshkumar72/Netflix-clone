2395bc66548ef8abe48beb1d5c1e367a1f80b1cc
Lokesh kumar singh <lokeshbsr544@gmail.com>
2026-08-29 16:36:08 +0530

    Update Netflix clone

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useRef } from 'react'
+import { Link } from 'react-router-dom'; // Import Link from react-router-dom
 import './Navbar.css'
 import logo from '../../assets/logo.png'
 import search_icon from '../../assets/search_icon.svg'
@@ -6,8 +7,8 @@ import bell_icon from '../../assets/bell_icon.svg'
 import profile_img from'../../assets/profile_img.png'
 import caret_icon from '../../assets/caret_icon.svg'
 import { logout } from '../../firebase'
-const Navbar = () => {
 
+const Navbar = () => {
   const navRef= useRef();
   useEffect(()=>{
     window.addEventListener('scroll',()=>{
@@ -18,33 +19,35 @@ const Navbar = () => {
       }
     })
   },[])
+
   return (
     <div ref={navRef} className='navbar'>
       <div className="navbar-left">
         <img src={logo} alt=''/>
         <ul>
-        <li>Home</li>
-        <li>TV Shows</li>
-        <li>Movies</li>
-        <li>New & Popular</li>
-        <li>My List</li>
-        <li>Browse by Languages</li>
+          <li>Home</li>
+          <li>TV Shows</li>
+          <li>Movies</li>
+          <li>New & Popular</li>
+          <li>My List</li>
+          <li>Browse by Languages</li>
+{/* <li><Link to="/login">Login</Link></li> */} {/* Remove Login link */}
+{/* <li><Link to="/signup">Sign Up</Link></li> */} {/* Remove Sign Up link */}
         </ul>
       </div>
       <div className="navbar-right">
         <img src={search_icon}alt='' className='icons'/>
-      <p>Children</p>
-      <img src={bell_icon} alt='' className='icons'/>
-      <div className="navbar-profile">
-        <img src={profile_img} alt='' className='profile'/>
-        <img src={caret_icon} alt='dropdown' className='dropdown-icon'/>
-        <div className="dropdown">
-          <p onClick={() => { logout() }}>Sign Out of Netflix</p>
+        <p>Children</p>
+        <img src={bell_icon} alt='' className='icons'/>
+        <div className="navbar-profile">
+          <img src={profile_img} alt='' className='profile'/>
+          <img src={caret_icon} alt='dropdown' className='dropdown-icon'/>
+          <div className="dropdown">
+            <p onClick={() => { logout() }}>Sign Out of Netflix</p>
+          </div>
         </div>
       </div>
-          </div>
-      </div>
-  
+    </div>
   )
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,53 +1,55 @@
-
 import { initializeApp } from "firebase/app";
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword, signOut } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
+import {
+  createUserWithEmailAndPassword,
+  getAuth,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 import { addDoc, collection, getFirestore } from "firebase/firestore";
 import { toast } from "react-toastify";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDqiOhl600NUafbZqWC0AOwJTuzaXslF1c",
-  authDomain: "netflix-clone-26e24.firebaseapp.com",
-  projectId: "netflix-clone-26e24",
-  storageBucket: "netflix-clone-26e24.firebasestorage.app",
-  messagingSenderId: "737503559485",
-  appId: "1:737503559485:web:5be09ff35b13fa8d87cf54"
+  apiKey: "AIzaSyD-eYogqKdZDpQ_n4ziQoWJ8hf6JeaUE6A",
+  authDomain: "netflix-clone-dd625.firebaseapp.com",
+  projectId: "netflix-clone-dd625",
+  storageBucket: "netflix-clone-dd625.firebasestorage.app",
+  messagingSenderId: "607988709897",
+  appId: "1:607988709897:web:054a11000fe11e8ba916d8",
+  measurementId: "G-4DMRJJQKZ5",
 };
 
-
 const app = initializeApp(firebaseConfig);
-const auth =getAuth(app);
-const db =getFirestore(app);
+const analytics = getAnalytics(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
 
+const signup = async (name, email, password) => {
+  try {
+    const res = await createUserWithEmailAndPassword(auth, email, password);
+    const user = res.user;
+    await addDoc(collection(db, "user"), {
+      uid: user.uid,
+      name,
+      authProvider: "local",
+      email,
+    });
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
 
-const signup = async (name, email, password)=>{
-    try {
-         const res = await createUserWithEmailAndPassword(auth,email,password);
-         const user = res.user;
-         await addDoc (collection(db,"user"),{
-            uid: user.uid,
-            name ,
-            authProvider:"local",
-            email,
-         });
-    }catch(error){
-        console.log(error);
-       toast.error(error.code.split('/')[1].split('-').join(" "));
-    }
-
-}
-
-const login =async(email, password)=>{
-    try{
-       await signInWithEmailAndPassword(auth,email,password);
-        }catch(error){
-            console.log(error);
-            toast.error(error.code.split('/')[1].split('-').join(" "));
-            
-    }
-
-
-}
-const logout =()=>{
-    signOut(auth);
-}
-export{auth, db, login, signup, logout};
+const login = async (email, password) => {
+  try {
+    await signInWithEmailAndPassword(auth, email, password);
+  } catch (error) {
+    console.log(error);
+    toast.error(error.code.split("/")[1].split("-").join(" "));
+  }
+};
+const logout = () => {
+  signOut(auth);
+};
+export { auth, db, login, signup, logout };
